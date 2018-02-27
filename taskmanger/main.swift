fe6bac324d17374a5c3ebf9e128b41aa59e3e91e
Americo Lopez <AmericoLopez@interapts-macbook-pro-6.local>
2018-02-27 13:37:49 -0500

    Initial Commit

---
+++ taskmanger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  taskmanger
+//
+//  Created by Americo Lopez on 2/27/18.
+//  Copyright © 2018 Americo Lopez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
